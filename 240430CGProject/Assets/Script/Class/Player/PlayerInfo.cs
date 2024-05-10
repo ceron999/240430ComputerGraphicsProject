cb9a9ff42137bdb9908908280cd9eff0a4d7a4e1
CeronMoa <56114106+ceron999@users.noreply.github.com>
2024-05-10 21:34:36 +0900

    240510/정윤석/PlayerInfo 회의대로 수정
    1. Player가 재화를 획득하는 것이 아닌 경험치를 획득하여 level을 올리는 방식으로 데이터 수정

--- 240430CGProject/Assets/Script/Class/Player/PlayerInfo.cs
+++ 240430CGProject/Assets/Script/Class/Player/PlayerInfo.cs
@@ -5,23 +5,27 @@ using UnityEngine;
 [System.Serializable]
 public class PlayerInfo
 {
-    public int money;                       //보유 금액
+    public int level;                       //현재 레벨
+    public float exp;                       //현재 경험치
+    public float maxExp;                    //현재 경험치가 maxExp를 넘어가면 다음 레벨로 업한다. 
 
     public float[] runningRecord;           //달리기 기록
 
-    public int weaponReinforcementCount;    //무기 강화 횟수 (0~5)
-    public int armorReinforcementCount;     //방어구 강화 횟수 (0~5)
-    public int shoesReinforcementCount;     //신발 강화 횟수 (0~5)
+    public int hpReinforcementCount;                //체력 강화 횟수 
+    public int attackDamageReinforcementCount;      //공격력 강화 횟수 
+    public int speedReinforcementCount;             //속도 강화 횟수 
 
     public PlayerInfo()
     {
-        money = 0;
+        level = 1;
+        exp = 0;
+        maxExp = 1000;
 
         runningRecord = new float[3];
 
         //상점에서 구매하지 않은 상태로 초기화
-        weaponReinforcementCount = 0;
-        armorReinforcementCount = 0;
-        shoesReinforcementCount = 0;
+        hpReinforcementCount = 0;
+        attackDamageReinforcementCount = 0;
+        speedReinforcementCount = 0;
     }
 }
