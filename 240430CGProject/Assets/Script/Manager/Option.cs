e5851ed12f602efe0d46aa388ff6a3230f915cca
Gayeong <dbsrkdud11@gmail.com>
2024-05-17 14:41:40 +0900

    StatManager 구현
    StatManager의 statmanager, option 스크립트 구현 완료
    그래픽 요소 추가할 예정

--- 240430CGProject/Assets/Script/Manager/Option.cs
+++ 240430CGProject/Assets/Script/Manager/Option.cs
@@ -2,65 +2,77 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Option : MonoBehaviour
 {
-
-    
+    public StatManager statManager;
+    // 해당 옵션의 현재 강화 레벨 
     public TMP_Text currentLevelText;
     int currentLevelData;
 
-
+    // 스탯 포인트 
     public TMP_Text statPointText;
-    int statPoint;
+
+    public GameObject saveButton;
 
     void Start()
     {
-
+        // 현재 옵션의 레벨 정보 가져오기
         currentLevelText = transform.GetChild(1).GetComponent<TMP_Text>();
         currentLevelData = int.Parse(currentLevelText.text);
-        statPoint = int.Parse(statPointText.text);
+ 
         
 
     }
 
-    // 강화 버튼 누르면
+    // (+) 버튼 누르면
     public void OnClickLevelUpBtn()
     {
-        // 레벨 데이터, 텍스트 1 증가
-        currentLevelData += 1;
-        currentLevelText.text = currentLevelData.ToString();
-
-        // 스탯 포인트 1 감소
-        statPoint -= 1;
-        statPointText.text = statPoint.ToString();
-
-        // 저장 버튼 활성화 여부 검사
-       /* if (!statManager.saveBtn.activeSelf)
+        // 스탯 포인트가 있으면 실행
+        if (statManager.statPoint != 0)
         {
-            statManager.saveBtn.SetActive(true);
-        }
+            // 레벨 데이터, 텍스트 1 증가
+            currentLevelData += 1;
+            currentLevelText.text = currentLevelData.ToString();
 
-        statManager.ToggleBtnActive();*/
+            // 스탯 포인트 1 감소
+            statManager.statPoint -= 1;
+            statPointText.text = statManager.statPoint.ToString();
+
+            // 저장 버튼 활성화 여부 검사
+            if (saveButton.GetComponent<Button>().interactable == false)
+            {
+                saveButton.GetComponent<Button>().interactable = true;
+            }
+        }
+            
+        
     }
 
+    // (-) 버튼 누르면
     public void OnClickLevelDownBtn()
     {
-        // 레벨 데이터, 텍스트 1 감소
-        currentLevelData -= 1;
-        currentLevelText.text = currentLevelData.ToString();
+        // 현재 레벨이 양수일 때만 실행
+        if (currentLevelData != 0) {
+            // 레벨 데이터, 텍스트 1 감소
+            currentLevelData -= 1;
+            currentLevelText.text = currentLevelData.ToString();
 
-        // 스탯 포인트 1 증가
-        statPoint += 1;
-        statPointText.text = statPoint.ToString();
+            // 스탯 포인트 1 증가
+            statManager.statPoint += 1;
+            statPointText.text = statManager.statPoint.ToString();
 
-        // 저장 버튼 활성화 여부 검사
-/*        if (!statManager.saveBtn.activeSelf)
-        {
-            statManager.saveBtn.SetActive(true);
+            // 저장 버튼 활성화 여부 검사
+            if (saveButton.GetComponent<Button>().interactable == false)
+            {
+                saveButton.GetComponent<Button>().interactable = true;
+            }
+
+     
         }
+           
 
-        statManager.ToggleBtnActive();*/
     }
 
 
--- 240430CGProject/Assets/Script/Manager/StatManager.cs
+++ 240430CGProject/Assets/Script/Manager/StatManager.cs
@@ -4,12 +4,14 @@ using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class StatManager : MonoBehaviour
 {
     // statManager 인스턴스 생성
     public static StatManager statManager;
 
+    // 여기 인스턴스 지정 안 돼서 오류 
     PlayerInfo PlayerInfo;
 
     int currentHpLevel;
@@ -20,14 +22,21 @@ public class StatManager : MonoBehaviour
     public TMP_Text damageLevelText;
     public TMP_Text speedLevelText;
 
-    // 버튼 담은 배열 만들어서 <Button> 컴포넌트 찾아서 순회하며 비활성화
+    // 레벨 +, - 버튼 담을 배열 
     GameObject[] optionButtons;
 
-    // 현재 사용할 수 있는 스탯 포인트 -> PlayerInfo 정보에 추가해야함
+    // 현재 사용할 수 있는 스탯 포인트 
+    // Playerinfo에 추가해야 함
+    // int statPoint = PlayerInfo.statPoint 
+    public int statPoint;
     public TMP_Text statPointText;
-    public int statPoint; 
+  
 
-    public GameObject saveBtn;
+    // 저장 버튼 
+    public GameObject saveButton;
+
+    // 뒤로가기 버튼
+    public GameObject goBackButton;
 
     void Awake()
     {
@@ -58,36 +67,42 @@ public class StatManager : MonoBehaviour
         // 업다운 버튼 배열(활성화 토글 목적)
         optionButtons = GameObject.FindGameObjectsWithTag("button");
 
-        //foreach (GameObject button in optionButtons)
-        // { Debug.Log(button.name) ; }
+        // 저장 버튼 비활성화로 초기화
+        saveButton.GetComponent<Button>().interactable = false;
 
+        // 강화 버튼 활성화 여부 검사
         ToggleBtnActive();
     }
 
     // 저장 버튼 눌렀을 시 
-    void OnClickSaveBtn()
+    public void OnClickSaveBtn()
     {
 
    
         // 변경된 레벨 및 스탯 포인트 PlayerInfo 에 저장 
-        PlayerInfo.hpReinforcementCount = int.Parse(hpLevelText.text);
-        PlayerInfo.attackDamageReinforcementCount = int.Parse(hpLevelText.text);
-        PlayerInfo.speedReinforcementCount = int.Parse(speedLevelText.text);
-        //PlayerInfo.statPoint = int.Parse(statPoint);
-        saveBtn.SetActive(false);
+        //PlayerInfo.hpReinforcementCount = int.Parse(hpLevelText.text);
+        //PlayerInfo.attackDamageReinforcementCount = int.Parse(hpLevelText.text);
+        //PlayerInfo.speedReinforcementCount = int.Parse(speedLevelText.text);
+        //PlayerInfo.statPoint = statPoint;
 
         // 버튼 활성화 검사
         ToggleBtnActive();
-        
+
+        // 저장 버튼 비활성화
+        saveButton.GetComponent<Button>().interactable = false;
+
 
     }
 
     // 버튼 활성화 토글
     public void ToggleBtnActive()
     {
+        Debug.Log("실행!");
+
         // 스탯 포인트 0이면
         if (statPoint <= 0)
         {
+            Debug.Log("비활성화!");
             // 버튼 비활성화
            foreach (GameObject button in optionButtons)
             {
@@ -100,13 +115,22 @@ public class StatManager : MonoBehaviour
         // 스탯 포인트 1 이상이면
         else 
         {
+            Debug.Log("활성화!");
             // 버튼 활성화
-            
-     
-  
+            foreach (GameObject button in optionButtons)
+            {
+                button.GetComponent<Button>().interactable = true;
+
+               
+            }
         }
 
        
+    }
+
+    public void GoBack()
+    {
+        SceneManager.LoadScene("StartScene");
     }
 
 
