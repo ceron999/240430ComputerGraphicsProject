15df511a025abf4449e658308d478a12ffca46a0
Gayeong <dbsrkdud11@gmail.com>
2024-05-17 20:55:54 +0900

    랭킹 구현
    랭킹 구현

--- 240430CGProject/Assets/Script/Manager/RankingManager.cs
+++ 240430CGProject/Assets/Script/Manager/RankingManager.cs
@@ -1,16 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
 
 public class RankingManager : MonoBehaviour
 {
+    // 기록 불러오기 
+    PlayerInfo playerInfo;
+    float[] runningRecordRanking;
+
+    // 1, 2, 3등 텍스트 
+    public TMP_Text firstPlace;
+    public TMP_Text secondPlace;
+    public TMP_Text thirdPlace;
+
     private void Start()
     {
-    
+        // 1, 2, 3등 화면에 출력 
+        runningRecordRanking = playerInfo.runningRecord;
+        firstPlace.text = runningRecordRanking[0].ToString() + "m";
+        secondPlace.text = runningRecordRanking[1].ToString() + "m";
+        thirdPlace.text = runningRecordRanking[2].ToString() + "m";
+        
     }
 
-    void PlusCount()
+    public void GoBack()
     {
-       
+        SceneManager.LoadScene("StartScene");
     }
+
+   
 }
